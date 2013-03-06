a6dee24a183bfb379e0b5fee8fb7ac7d70394672
Jim <jim.gomez.48@gmail.com>
2013-03-06 15:31:42 -0800

    Added a menu bar to the JFrame in the north sector.

--- gomez_james_gui_p2/src/com/jamesgomez/minesweeper/GamePanel.java
+++ gomez_james_gui_p2/src/com/jamesgomez/minesweeper/GamePanel.java
@@ -1,12 +1,20 @@
 package com.jamesgomez.minesweeper;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel {
 
+    private static JFrame frame;
     private Board gameBoard;
     private Display display;
+
+    public enum GameDifficulty {BEGINNER, INTERMEDIATE, EXPERT, CUSTOM };
+    private GameDifficulty difficulty;
 
     public GamePanel() {
         super(new BorderLayout(), true);
@@ -14,6 +22,8 @@ public class GamePanel extends JPanel {
         setLayout(new BorderLayout(5, 5));
         gameBoard = new Board(new GridLayout(2, 2));
         display = new Display(new BorderLayout(5, 5));
+
+        difficulty = GameDifficulty.BEGINNER;
 
         gameBoard.setBackground(Color.red);
         display.setPreferredSize(new Dimension(300, 30));
@@ -24,24 +34,82 @@ public class GamePanel extends JPanel {
     }
 
     public static JMenuBar createMenuBar() {
-        //create menu objects
         JMenuBar menuBar = new JMenuBar();
         JMenu gameMenu = new JMenu("Game");
+        JMenuItem newGame = new JMenuItem("New Game");
+        JMenuItem exit = new JMenuItem("Exit");
+        JMenu difficultyMenu = new JMenu("Difficulty");
         JMenuItem beginner = new JMenuItem("Beginner");
         JMenuItem intermediate = new JMenuItem("Intermediate");
         JMenuItem expert = new JMenuItem("Expert");
+        JMenuItem custom = new JMenuItem("Custom");
+
+        //TODO implement correct action on menu item clicks
+        beginner.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Beginner");
+            }
+        });
+
+        intermediate.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Intermediate");
+            }
+        });
+
+        expert.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Expert");
+            }
+        });
+
+        custom.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Custom");
+            }
+        });
+
+        newGame.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "New Game");
+            }
+        });
+
+        exit.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+
         //add menu items to game menu
-        gameMenu.add(beginner);
-        gameMenu.add(intermediate);
-        gameMenu.add(expert);
-        //add game menu to menu bar
+        gameMenu.add(newGame);
+        gameMenu.addSeparator();
+        gameMenu.add(exit);
+        difficultyMenu.add(beginner);
+        difficultyMenu.add(intermediate);
+        difficultyMenu.add(expert);
+        difficultyMenu.addSeparator();
+        difficultyMenu.add(custom);
         menuBar.add(gameMenu);
+        menuBar.add(difficultyMenu);
 
         return menuBar;
     }
 
     public static void start() {
-        JFrame frame = new JFrame("Minesweeper");
+        frame = new JFrame("Minesweeper");
         GamePanel gamePanel = new GamePanel();
         frame.add(createMenuBar(), BorderLayout.NORTH);
         frame.add(gamePanel, BorderLayout.CENTER);
