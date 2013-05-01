8f9881d7ba7071254cdfa2ad880ffec5d7422866
Jim <jim.gomez.48@gmail.com>
2013-05-01 16:40:42 -0700

    Got a wpf window working

--- gomez_james_gui_p3/gomez_james_gui_p3/MandelbrotWindow.cs
+++ gomez_james_gui_p3/gomez_james_gui_p3/MandelbrotWindow.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace gomez_james_gui_p3
 {
     class MandelbrotWindow: Window
     {
+        private Canvas canvas;
 
+        public MandelbrotWindow(): base()
+        {
+            canvas = new Canvas();
+            this.Content = canvas;
+        }
+
+        [STAThread]
         public static void Main(String[] args)
         {
-            
+            MandelbrotWindow window = new MandelbrotWindow();
+            window.Show();
+            Application app = new Application();
+            app.Run();            
         }
     }
 }
