04054c881eb2c710f1e826190055b82ae2aa68bc
Jim <jim.gomez.48@gmail.com>
2013-04-30 17:03:19 -0700

    Initial commit for final GUI project

---
+++ gomez_james_gui_p3/gomez_james_gui_p3/MandelbrotWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace gomez_james_gui_p3
+{
+    class MandelbrotWindow: Window
+    {
+
+        public static void Main(String[] args)
+        {
+            
+        }
+    }
+}
