7999a1252ea82edb84181bc9afee72556db78887
Jim <jim.gomez.48@gmail.com>
2013-05-01 17:18:12 -0700

    started MandelbrotGrid skeleton class

---
+++ gomez_james_gui_p3/gomez_james_gui_p3/MandelbrotGrid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gomez_james_gui_p3
+{
+    class MandelbrotGrid
+    {
+        // primary grid properties 
+        double xstart, ystart, width, height;
+        int rows, cols;
+        int[,] data;
+        int maxiterations;
+        double maxmodulus;
+        // secondary properties calculated from primary properties 
+        int xintervals, yintervals;
+        double dx, dy;
+
+        public MandelbrotGrid(double xs, double ys, double w, 
+            double h, int r, int c, int mi, double mm)
+        {            
+            cols = 0; rows = 0;
+            
+            //TODO direct assignment properties 
+
+            // calculated properties 
+            xintervals = cols - 1;
+            yintervals = rows - 1;
+            dx = width / xintervals;
+            dy = height / yintervals;
+        }
+    }
+}
--- gomez_james_gui_p3/gomez_james_gui_p3/MandelbrotWindow.cs
+++ gomez_james_gui_p3/gomez_james_gui_p3/MandelbrotWindow.cs
@@ -5,26 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace gomez_james_gui_p3
 {
     class MandelbrotWindow: Window
     {
         private Canvas canvas;
+        private Image image;
+        private BitmapSource bmpsource;
 
         public MandelbrotWindow(): base()
         {
             canvas = new Canvas();
             this.Content = canvas;
+
+            image = new Image();
+            //bmpsource = BitmapSource.Create(
+            //    int, 
+            //    int, 
+            //    double, 
+            //    double, 
+            //    pixelFormat, 
+            //    BitmapPalette, 
+            //    Array, 
+            //    int
+            //);
         }
 
         [STAThread]
         public static void Main(String[] args)
         {
-            MandelbrotWindow window = new MandelbrotWindow();
-            window.Show();
+            MandelbrotWindow window = new MandelbrotWindow();         
             Application app = new Application();
-            app.Run();            
+            app.Run(window);            
         }
     }
 }
