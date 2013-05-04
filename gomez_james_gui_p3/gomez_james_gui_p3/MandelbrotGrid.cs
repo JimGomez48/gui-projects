4a4a0aaa7844297307a1494ace01fa06a2fde0e8
Jim <jim.gomez.48@gmail.com>
2013-05-04 15:35:03 -0700

    WIP

--- gomez_james_gui_p3/gomez_james_gui_p3/MandelbrotGrid.cs
+++ gomez_james_gui_p3/gomez_james_gui_p3/MandelbrotGrid.cs
@@ -9,31 +9,102 @@ namespace gomez_james_gui_p3
 {
     class MandelbrotGrid
     {
-        // primary grid properties 
-        double xStart, yStart, width, height;
-        int rows, cols;
         int[,] data;
-        int maxIterations;
-        double maxModulus;
-        // secondary properties calculated from primary properties 
-        int xIntervals, yIntervals;
+        private double xStart, yStart, width, height;
+
+        private readonly int rows;
+        private readonly int cols;
+        private readonly int maxIterations;
+        private readonly double maxModulus;
+
+        int xIntervals;
+        int yIntervals;
         double dx, dy;
 
-        public MandelbrotGrid(double xs, double ys, double w,
-            double h, int r, int c, int mi, double mm) {
-            cols = 0; rows = 0;
+        /// <summary>
+        /// Creates a new two-dimensional Mandelbgrot grid representing a complex plain
+        /// </summary>
+        /// <param name="xStart">The x starting position</param>
+        /// <param name="yStart">The y starting position</param>
+        /// <param name="width">The width of the Mandelbrot grid</param>
+        /// <param name="height">the height of the Mandelbrot grid</param>
+        /// <param name="numRows">the number of rows to be contained within the Mandelbrot grid</param>
+        /// <param name="numCols">the number of columns to be contained within the Mandelbrot grid</param>
+        /// <param name="maxIterations">the maximum amount of iterations before we consider any complex point to diverge</param>
+        /// <param name="maxModulus">the maximum complex modulus value before we consider any complex point to diverge</param>
+        public MandelbrotGrid(double xStart, double yStart, double width, double height,
+            int numRows, int numCols, int maxIterations, double maxModulus) {
 
-            //TODO direct assignment properties 
-
-            Complex c1 = new Complex(1, 1);
-            Complex c2 = new Complex(2, 4);
-            Console.WriteLine(c1 + c2);
+            this.xStart = xStart;
+            this.yStart = yStart;
+            this.width = width;
+            this.height = height;
+            this.rows = numRows;
+            this.cols = numCols;
+            this.maxIterations = maxIterations;
+            this.maxModulus = maxModulus;
+            data = new int[rows, cols];
 
             // calculated properties 
             xIntervals = cols - 1;
-            yIntervals = rows - 1;
+            yIntervals = numRows - 1;
             dx = width / xIntervals;
             dy = height / yIntervals;
+
+            generateCounts();
+            printData();
+            //testPrint();
+        }
+
+        private void generateCounts() {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    Complex c = toComplex(i, j);
+                    Complex z = new Complex(0, 0);
+
+                    int iterations = 0;
+                    while (true) {
+                        z = z * z + c;
+                        iterations++;
+                        if (z.Magnitude * z.Magnitude >= maxModulus || iterations >= maxIterations)
+                            break;
+                    }
+
+                    if (iterations >= maxIterations)
+                        data[i, j] = 0;
+                    else
+                        data[i, j] = iterations;
+                }
+            }
+        }
+
+        private Complex toComplex(int row, int col) {
+            double x, y;
+            x = (double)(col - cols / 2) * 0.1;
+            y = (double)(row - row / 2) * 0.1;
+
+            return new Complex(x, y);
+        }
+
+        private void printData() {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    Console.Write(data[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //for testing only
+        private void testPrint() {
+            Complex z;
+
+            z = new Complex(0.1, 0.1); Console.WriteLine(z + " " + z.Magnitude * z.Magnitude);
+            z = new Complex(0.1, 0.12); Console.WriteLine(z + " " + z.Magnitude * z.Magnitude);
+            z = new Complex(0.0956, 0.124); Console.WriteLine(z + " " + z.Magnitude * z.Magnitude);
+            z = new Complex(0.09376, 0.1237088); Console.WriteLine(z + " " + z.Magnitude * z.Magnitude);
+            z = new Complex(0.09348, 0.123198705499136); Console.WriteLine(z + " " + z.Magnitude * z.Magnitude);
+            z = new Complex(0.09362, 0.12303); Console.WriteLine(z + " " + z.Magnitude * z.Magnitude);            
         }
     }
 }
--- gomez_james_gui_p3/gomez_james_gui_p3/MandelbrotWindow.cs
+++ gomez_james_gui_p3/gomez_james_gui_p3/MandelbrotWindow.cs
@@ -14,12 +14,13 @@ namespace gomez_james_gui_p3
     {
         private Canvas canvas;
         private Image image;
-        private BitmapSource bmpsource;
+        private BitmapSource bmpSource;
+        private MandelbrotGrid grid;
 
         public MandelbrotWindow()
             : base() {
             canvas = new Canvas();
-            this.Content = canvas;
+            this.Content = canvas;            
 
             image = new Image();
             //bmpsource = BitmapSource.Create(
@@ -32,15 +33,7 @@ namespace gomez_james_gui_p3
             //    Array, 
             //    int
             //);
-
-            Complex x = new Complex(0.1, 0.1);
-            Complex y = new Complex(0.1, 0.12);
-            Console.WriteLine(x); 
-            Console.WriteLine(y);
-
-            //x.Mod() == x.Mag * x.Mag
-            Console.WriteLine(x.Magnitude * x.Magnitude);
-            Console.WriteLine(y.Magnitude * y.Magnitude);
+            grid = new MandelbrotGrid(0, 0, 720, 720, 100, 100, 1000, 1000);
         }
 
         public Canvas Canvas {
@@ -51,7 +44,7 @@ namespace gomez_james_gui_p3
         public static void Main(String[] args) {
             MandelbrotWindow window = new MandelbrotWindow();
             Application app = new Application();
-            app.Run(window);
+            //app.Run(window);
         }
     }
 }
